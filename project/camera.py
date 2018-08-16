c1de58f276579c57ab82fe3c7a604148f00d5156
Lorcan Coyle <lorcan@gmail.com>
2018-08-16 22:38:22 +0100

    Testing picamera

--- project/camera.py
+++ project/camera.py
@@ -1,5 +1,27 @@
+import picamera
+import time
+from datetime import datetime
+
+def now_string():
+  return datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+
 def start():
   print("starting camera!")
 
 def capture():
-  print("Capture")
+  timestamp = now_string()
+  with picamera.PiCamera() as camera:
+    camera.annotate_text = timestamp
+    filename = '{}/{}.jpg'.format('project/static/photos', timestamp)
+    camera.capture(filename)
+    return filename
+
+def video(seconds=10):
+  timestamp = now_string()
+  with picamera.PiCamera() as camera:
+    camera.resolution = (640,480)
+    filename = '{}/{}.mjpeg'.format('project/static/videos', timestamp)
+    camera.start_recording(filename)
+    camera.wait_recording(seconds)
+    camera.stop_recording()
+    return filename
--- project/views.py
+++ project/views.py
@@ -1,24 +1,21 @@
 import os
 
 from flask import Flask, flash, redirect, render_template, url_for
-
 import camera
-
 from . import app
 
-
-def get_photos():
-  filenames = os.listdir('project/static/photos')
-  photo_filenames = []
-  for filename in filenames:
-    if filename.endswith(".png"):
-      photo_filenames.append('photos/' + filename)
-  return photo_filenames
+def get_paths_from_directory_with_suffix(directory, suffix):
+  paths = []
+  for filename in os.listdir('project/static/' + directory):
+    if filename.endswith(suffix):
+      paths.append(directory + '/' + filename)
+  return paths
 
 @app.route("/")
 def index():
-  photos = get_photos()
-  return render_template('index.html', photos=photos)
+  photos = get_paths_from_directory_with_suffix('photos', '.jpg')
+  videos = get_paths_from_directory_with_suffix('videos', 'mjpeg')
+  return render_template('index.html', photos=photos, videos=videos)
 
 @app.route("/capture/", methods=['POST'])
 def capture():
@@ -30,4 +27,10 @@ def capture():
 def start():
   camera.start()
   flash('Successfully started the camera')
+  return redirect(url_for('index'))
+
+@app.route("/video/", methods=['POST'])
+def video():
+  camera.video(10)
+  flash('Successfully took a video')
   return redirect(url_for('index'))
